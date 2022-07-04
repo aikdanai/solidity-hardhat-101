aa237275852851d0abe37d4c3d8a750e3284244d
Aikdanai Sidhikosol <aikdanai@cleverse.com>
2022-07-04 22:41:53 +1000

    chore: add hardhat network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,7 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    hardhat: {},
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
